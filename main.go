038aed4563b1b1c6e369ccd4d073b6a5643f7cfd
Nikolay <nikolay.dubina.pub@gmail.com>
2022-11-10 00:52:38 +0800

    packages

--- main.go
+++ main.go
@@ -2,115 +2,15 @@ package main
 
 import (
 	"flag"
-	"go/ast"
 	"go/parser"
 	"go/printer"
 	"go/token"
 	"log"
 	"os"
 
-	"golang.org/x/tools/go/ast/astutil"
-
 	"github.com/nikolaydubina/go-instrument/instrument"
+	"github.com/nikolaydubina/go-instrument/processor"
 )
-
-const (
-	contextName    = "ctx"
-	contextPackage = "context"
-	contextType    = "Context"
-	errorName      = "err"
-	errorType      = `error`
-)
-
-func methodReceiverTypeName(spec ast.FuncDecl) string {
-	// function
-	if spec.Recv == nil {
-		return ""
-	}
-	// method
-	for _, v := range spec.Recv.List {
-		if v == nil {
-			continue
-		}
-		t := v.Type
-		// poitner receiver
-		if v, ok := v.Type.(*ast.StarExpr); ok {
-			t = v.X
-		}
-		// value/pointer receiver
-		if v, ok := t.(*ast.Ident); ok {
-			return v.Name
-		}
-	}
-	return ""
-}
-
-func functionName(spec ast.FuncDecl) string {
-	if spec.Name == nil {
-		return ""
-	}
-	return spec.Name.Name
-}
-
-func spanName(methodReciverName, functionName string) string {
-	if methodReciverName == "" {
-		return functionName
-	}
-	return methodReciverName + "." + functionName
-}
-
-func isContext(e ast.Field) bool {
-	// anonymous arg
-	// multilple symbols
-	// strange symbol
-	if len(e.Names) != 1 || e.Names[0] == nil {
-		return false
-	}
-	if e.Names[0].Name != contextName {
-		return false
-	}
-
-	pkg := ""
-	sym := ""
-
-	if se, ok := e.Type.(*ast.SelectorExpr); ok && se != nil {
-		if v, ok := se.X.(*ast.Ident); ok && v != nil {
-			pkg = v.Name
-		}
-		if v := se.Sel; v != nil {
-			sym = v.Name
-		}
-	}
-
-	return pkg == contextPackage && sym == contextType
-}
-
-func isError(e ast.Field) bool {
-	// anonymous arg
-	// multilple symbols
-	// strange symbol
-	if len(e.Names) != 1 || e.Names[0] == nil {
-		return false
-	}
-	if e.Names[0].Name != errorName {
-		return false
-	}
-
-	if v, ok := e.Type.(*ast.Ident); ok && v != nil {
-		return v.Name == errorType
-	}
-
-	return false
-}
-
-const (
-	frameworkOpenTelemetry = "open-telemetry"
-)
-
-type Instrumenter interface {
-	Imports() []string
-	PrefixStatements(spanName string, hasError bool) []ast.Stmt
-}
 
 func main() {
 	var (
@@ -118,85 +18,39 @@ func main() {
 		overwrite  bool
 		tracerName string
 		verbosity  int
-		framework  string
 	)
 	flag.StringVar(&fileName, "file", "", "go file to instrument")
 	flag.StringVar(&tracerName, "tracer-name", "app", "name of tracer")
 	flag.BoolVar(&overwrite, "w", false, "overwrite original file")
 	flag.IntVar(&verbosity, "v", 0, "verbositry of STDERR logs")
-	flag.StringVar(&framework, "framework", frameworkOpenTelemetry, `framework for instrumentation ("`+frameworkOpenTelemetry+`")`)
 	flag.Parse()
-
-	fset := token.NewFileSet()
 
 	if fileName == "" {
 		log.Fatalln("missing arg: file name")
 	}
+
+	fset := token.NewFileSet()
 
 	file, err := parser.ParseFile(fset, fileName, nil, 0)
 	if err != nil {
 		log.Fatalf("can not parse input file: %s", err)
 	}
 
-	var instrumenter Instrumenter = &instrument.OpenTelemetry{
+	var instrumenter processor.Instrumenter = &instrument.OpenTelemetry{
 		TracerName:  tracerName,
-		ContextName: contextName,
-		ErrorName:   errorName,
+		ContextName: "ctx",
+		ErrorName:   "error",
 	}
-
-	astutil.Apply(file, nil, func(c *astutil.Cursor) bool {
-		if c == nil {
-			return true
-		}
-
-		fn, ok := c.Node().(*ast.FuncDecl)
-		if !ok || fn == nil {
-			return true
-		}
-
-		spanName := spanName(methodReceiverTypeName(*fn), functionName(*fn))
-
-		hasContext := false
-		hasError := false
-
-		if t := fn.Type; t != nil {
-			if ps := t.Params; ps != nil {
-				for _, q := range ps.List {
-					if q == nil {
-						continue
-					}
-					hasContext = hasContext || isContext(*q)
-				}
-			}
-
-			if rs := t.Results; rs != nil {
-				for _, q := range rs.List {
-					if q == nil {
-						continue
-					}
-					hasError = hasError || isError(*q)
-				}
-			}
-		}
-
-		if verbosity > 0 {
-			log.Printf("%s: has_context(%t) has_error(%t)\n", spanName, hasContext, hasError)
-		}
-
-		if !hasContext {
-			return true
-		}
-
-		fn.Body.List = append(instrumenter.PrefixStatements(spanName, hasError), fn.Body.List...)
-
-		c.Replace(fn)
-
-		return true
-	})
-
-	for _, q := range instrumenter.Imports() {
-		astutil.AddImport(fset, file, q)
+	p := processor.Processor{
+		Instrumenter:   instrumenter,
+		SpanName:       processor.BasicSpanName,
+		ContextName:    "ctx",
+		ContextPackage: "context",
+		ContextType:    "Context",
+		ErrorName:      "err",
+		ErrorType:      `error`,
 	}
+	p.Process(fset, file)
 
 	printer.Fprint(os.Stdout, fset, file)
 }
---
+++ processor/processor.go
@@ -0,0 +1,158 @@
+package processor
+
+import (
+	"go/ast"
+	"go/token"
+
+	"golang.org/x/tools/go/ast/astutil"
+)
+
+// Instrumenter adds instrumentation code to methods and functions and all supporting muttations to file.
+type Instrumenter interface {
+	Imports() []string
+	PrefixStatements(spanName string, hasError bool) []ast.Stmt
+}
+
+func BasicSpanName(receiver, method string) string {
+	if receiver == "" {
+		return method
+	}
+	return receiver + "." + method
+}
+
+// Processor traverses AST, collects details on funtions and methods, and invokes Instrumenter.
+type Processor struct {
+	Instrumenter   Instrumenter
+	SpanName       func(receiver, method string) string
+	ContextName    string
+	ContextPackage string
+	ContextType    string
+	ErrorName      string
+	ErrorType      string
+}
+
+func (p Processor) methodReceiverTypeName(spec ast.FuncDecl) string {
+	// function
+	if spec.Recv == nil {
+		return ""
+	}
+	// method
+	for _, v := range spec.Recv.List {
+		if v == nil {
+			continue
+		}
+		t := v.Type
+		// poitner receiver
+		if v, ok := v.Type.(*ast.StarExpr); ok {
+			t = v.X
+		}
+		// value/pointer receiver
+		if v, ok := t.(*ast.Ident); ok {
+			return v.Name
+		}
+	}
+	return ""
+}
+
+func (p Processor) functionName(spec ast.FuncDecl) string {
+	if spec.Name == nil {
+		return ""
+	}
+	return spec.Name.Name
+}
+
+func (p Processor) isContext(e ast.Field) bool {
+	// anonymous arg
+	// multilple symbols
+	// strange symbol
+	if len(e.Names) != 1 || e.Names[0] == nil {
+		return false
+	}
+	if e.Names[0].Name != p.ContextName {
+		return false
+	}
+
+	pkg := ""
+	sym := ""
+
+	if se, ok := e.Type.(*ast.SelectorExpr); ok && se != nil {
+		if v, ok := se.X.(*ast.Ident); ok && v != nil {
+			pkg = v.Name
+		}
+		if v := se.Sel; v != nil {
+			sym = v.Name
+		}
+	}
+
+	return pkg == p.ContextPackage && sym == p.ContextType
+}
+
+func (p Processor) isError(e ast.Field) bool {
+	// anonymous arg
+	// multilple symbols
+	// strange symbol
+	if len(e.Names) != 1 || e.Names[0] == nil {
+		return false
+	}
+	if e.Names[0].Name != p.ErrorName {
+		return false
+	}
+
+	if v, ok := e.Type.(*ast.Ident); ok && v != nil {
+		return v.Name == p.ErrorType
+	}
+
+	return false
+}
+
+func (p Processor) Process(fset *token.FileSet, file *ast.File) {
+	astutil.Apply(file, nil, func(c *astutil.Cursor) bool {
+		if c == nil {
+			return true
+		}
+
+		fn, ok := c.Node().(*ast.FuncDecl)
+		if !ok || fn == nil {
+			return true
+		}
+
+		spanName := p.SpanName(p.methodReceiverTypeName(*fn), p.functionName(*fn))
+
+		hasContext := false
+		hasError := false
+
+		if t := fn.Type; t != nil {
+			if ps := t.Params; ps != nil {
+				for _, q := range ps.List {
+					if q == nil {
+						continue
+					}
+					hasContext = hasContext || p.isContext(*q)
+				}
+			}
+
+			if rs := t.Results; rs != nil {
+				for _, q := range rs.List {
+					if q == nil {
+						continue
+					}
+					hasError = hasError || p.isError(*q)
+				}
+			}
+		}
+
+		if !hasContext {
+			return true
+		}
+
+		fn.Body.List = append(p.Instrumenter.PrefixStatements(spanName, hasError), fn.Body.List...)
+
+		c.Replace(fn)
+
+		return true
+	})
+
+	for _, q := range p.Instrumenter.Imports() {
+		astutil.AddImport(fset, file, q)
+	}
+}
